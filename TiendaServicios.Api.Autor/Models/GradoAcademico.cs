e08d0cae487caff6d06bde5c4e0627155addf6ff
Jose Maria Farfan <magoalakai@gmail.com>
2022-06-09 18:10:18 +0200

    Add project files.

---
+++ TiendaServicios.Api.Autor/Models/GradoAcademico.cs
@@ -0,0 +1,12 @@
+﻿namespace TiendaServicios.Api.Autor.Models;
+
+public class GradoAcademico
+{
+    public int GradoAcademicoId { get; set; }
+    public string? Nombre { get; set; }
+    public string? CentroAcademico { get; set; }
+    public DateTime? FechaGrado { get; set; }
+    public int? AutorLibroId { get; set; }
+    public AutorLibro? AutorLibro { get; set; }
+    public Guid GradoAcademicoGuid { get; set; }
+}
---
+++ TiendaServicios.Api.Autor/Usings.cs
@@ -0,0 +1,7 @@
+﻿global using MediatR;
+global using Microsoft.EntityFrameworkCore;
+global using TiendaServicios.Api.Autor.Application;
+global using TiendaServicios.Api.Autor.Models;
+global using TiendaServicios.Api.Autor.Persistency;
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.AspNetCore.Mvc;
