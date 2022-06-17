0bd8ce0766e24afb2e8e29b2da03f64021d4e713
Jose Maria Farfan <magoalakai@gmail.com>
2022-06-17 12:53:00 +0200

    Libro Microservice running properly

--- TiendaLibreria.Client/Program.cs
+++ TiendaLibreria.Client/Program.cs
@@ -1,7 +1,4 @@
-﻿
-using TiendaServicios.Contracts.Protos.Dto.Libro.Response;
-
-Console.WriteLine("Server starting...");
+﻿Console.WriteLine("Server starting...");
 Thread.Sleep(1000);
 
 HttpClientHandler http_client_handler = new()
@@ -50,5 +47,55 @@ GetLibroRequest get_libro_request = new()
 GetLibroResponse get_libro_response = await client.GetLibroAsync(get_libro_request);
 Console.WriteLine($"GetLibroAsync Response Success: {get_libro_response.Success}");
 Console.WriteLine($"GetLibroAsync Response Model: {get_libro_response.LibroModel}");
+
+// GetAllLibrosAsync
+Console.WriteLine("GetAllLibrosAsync started...");
+Thread.Sleep(1000);
+
+GetAllLibrosRequest get_all_libros_request = new();
+LibroModelListResponse get_all_libros_response = await client.GetAllLibrosAsync(get_all_libros_request);
+
+Console.WriteLine($"GetAllLibrosAsync Response Success: {get_all_libros_response.Success}");
+Console.WriteLine($"GetAllLibrosAsync Response Model List:");
+
+foreach (LibroModel libro_model in get_all_libros_response.LibroModelList)
+{
+    Console.WriteLine(libro_model);
+}
+
+// UpdateLibroAsync
+Console.WriteLine("UpdateLibroAsync started...");
+Thread.Sleep(1000);
+
+LibroModel update_libro_model = new()
+{
+    Id = 4,
+    Titulo = "Tha jander clander",
+    AutorLibro = "63b9b893-c911-4927-9b44-194141fbf25b",
+    LibreriaMaterialGuid = Guid.NewGuid().ToString(),
+    FechaPublicacion = Timestamp.FromDateTime(DateTime.Now.ToUniversalTime()),
+};
+
+UpdateLibroRequest update_libro_request = new()
+{
+    LibroId = 4,
+    LibroModel = update_libro_model
+};
+
+UpdateLibroResponse update_libro_response = await client.UpdateLibroAsync(update_libro_request);
+Console.WriteLine($"UpdateLibroAsync Response Success: {update_libro_response.Success}");
+Console.WriteLine($"UpdateLibroAsync Response Model: {update_libro_response.LibroModel}");
+
+// DeleteLibroAsync
+Console.WriteLine("DeleteLibroAsync started...");
+Thread.Sleep(1000);
+
+DeleteLibroRequest delete_libro_request = new()
+{
+    LibroId = 4,
+};
+
+DeleteLibroResponse delete_libro_response = await client.DeleteLibroAsync(delete_libro_request);
+Console.WriteLine($"DeleteLibroAsync Response Success: {update_libro_response.Success}");
 
 Console.ReadLine();
